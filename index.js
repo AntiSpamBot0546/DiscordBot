261ca2ff78bbc39e13a39ea3e0b56a50c3af61e0
Arda0546 <Arda.sen0546@hotmail.com>
2020-05-14 16:05:10 +0200

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js")
+const bot = new Discord.Client();
+const prefix = "!";
+
+
+
+
+bot.on('ready', ()=>{
+    console.log("Bot is online!")
+   
+}) 
+
+
+bot.on('message', (message)=>{
+
+
+  
+
+   
+    
+  
+    
+});
+  
+
+
+
+
+
+bot.login(process.env.token);
